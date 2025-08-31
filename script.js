e0e713d4da40eabbe069e32f29313333a21126d6
Ahammed Hossain <ashikahmed3136@gmail.com>
2025-08-31 16:09:17 +0600

    Update js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let password = document.getElementById("Generate-box");
+let lengthValue = document.querySelector(".length-value");
+let lengthSlider = document.getElementById("lengthSlider");
+
+let Generate = document.querySelector('.generate');
+let Copy = document.querySelector('.copy');
+
+
