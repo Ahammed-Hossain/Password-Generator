438ae8f4bf1f81b81db1e3f791d98cb1f47044dd
Ahammed Hossain <ashikahmed3136@gmail.com>
2025-08-31 23:50:21 +0600

    Update html css js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-
 let password = document.getElementById("Generate-box");
-let lengthValue = document.querySelector(".length-value");
+let lengthValue = document.getElementById("length-value");
 let lengthSlider = document.getElementById("lengthSlider");
+
+lengthValue.innerText = lengthSlider.value;
+
+lengthSlider.addEventListener('input', (event) => {
+    lengthValue.innerHTML = event.target.value;
+});
+
+
+
+let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+let letters = chars.split("");
+
+function generatePassword() {
+  
+};
+
+let rando
 
 let Generate = document.querySelector('.generate');
 let Copy = document.querySelector('.copy');
 
+Generate.addEventListener('click', function () {
 
+});
